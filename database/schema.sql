b3ebe94ceb9cfcb47d023d9bcfa62f2659ee61ae
Srikanth Mangali <srikanthmangali@Srikanths-MacBook-Air.local>
2026-03-23 17:44:30 -0400

    Initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,20 @@
 CREATE TABLE users (
-user id int,
+user_id INT PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(50),
 email VARCHAR(20),
-password VARCHAR(20)
+password VARCHAR(20),
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+CREATE TABLE food_replacements (
+    replacement_id INT PRIMARY KEY AUTO_INCREMENT,
+    non_vegan_item VARCHAR(50),
+    vegan_alternatives VARCHAR(50),
+    nutrient_focus VARCHAR(50),
+    replacement_reason VARCHAR(50),
+    usage_type VARCHAR(50),
+    notes TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+

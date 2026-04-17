c3776f3d703f877739cef8f9359e97dd1f3d3d06
Bosu <badebosubabu@gmail.com>
2026-04-17 15:46:21 -0400

    update the routes

--- backend/src/routes/nutrientRoutes.js
+++ backend/src/routes/nutrientRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getNutrientsToWatch } = require("../controllers/nutrientController");
+const {
+  getNutrientsToWatch,
+  getPersonalizedNutrients,
+} = require("../controllers/nutrientController");
 
 const router = express.Router();
 
 router.get("/", getNutrientsToWatch);
+router.post("/recommend", getPersonalizedNutrients);
 
 module.exports = router;
